b6f506084802756bf12f0feff4e50cc104d48d24
Iuriiiii <alexandercasasnqn@gmail.com>
2024-10-18 23:56:24 -0300

    fix: remove index and slow types

--- src/singletons/serializables.singleton.ts
+++ src/singletons/serializables.singleton.ts
@@ -1,3 +1,4 @@
 // Copyright (c) 2024 Oscar Alexander Casas
 // Licensed under the Commercial Use License. For inquiries, contact alexandercasasnqn@gmail.com
-export const serializables = new Map();
+// deno-lint-ignore no-explicit-any
+export const serializables: Map<any, any> = new Map();
